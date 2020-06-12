3cb92f02f80022746252fe826af9c58d2c140385
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 05:48:31 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -7,7 +7,11 @@ class Band extends Component {
     return(
       <div>
         Band Component
+<<<<<<< HEAD
         <span>{this.props.bands}</span>
+=======
+        <span>{this.props.band.name}</span>
+>>>>>>> c8d6cda3ae563bb9a380561245251d0159f5b0c8
         <button onClick={() => this.props.delete(this.props.todo.id)}>DELETE</button>
       </div>
     );
@@ -15,7 +19,11 @@ class Band extends Component {
 };
 
 const mapDispatchToProps = dispatch => {
+<<<<<<< HEAD
   return {delete: band => dispatch({type: 'DELETE_BAND', payload: band})}
+=======
+  delete: band => dispatch({type: 'DELETE_BAND', payload: band})
+>>>>>>> c8d6cda3ae563bb9a380561245251d0159f5b0c8
 }
 
 export default Band;
