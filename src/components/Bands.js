463b6c60789e8f79607859937b5db4a22ffae37f
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 05:56:47 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,13 +2,7 @@ import React from 'react';
 import Band from './Band'
 
 const Bands = props => {
-  const bands = props.bands.map(band => <Band key={band.id} {...band} deleteBand={props.deleteBand}/>)
-
-  return (
-    <div>
-      {bands}
-    </div>
-  );
+const bands = props.bands.map(band => <Band key={}/>)
 
 };
 
