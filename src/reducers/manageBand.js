45db017c2f0e85bcb70b1fedaa73c57ed4261876
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 06:09:26 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,8 +3,8 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      const band = {id: Math.random(), }
-      return { ...state, bands: [...state.bands, action.name] }
+      const band = {id: Math.random(), name: action.name}
+      return { ...state, bands: [...state.bands, name] }
       
     case 'DELETE_BAND':
       return {bands: state.bands.filter(band => band.id !== action.payload)}
