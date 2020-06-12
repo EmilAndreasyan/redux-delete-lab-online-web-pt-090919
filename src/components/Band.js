9b08589ea7e1f5f65466c34822466e14a92d29f9
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 05:49:46 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
 
 class Band extends Component {
 
@@ -7,23 +6,13 @@ class Band extends Component {
     return(
       <div>
         Band Component
-<<<<<<< HEAD
         <span>{this.props.bands}</span>
-=======
         <span>{this.props.band.name}</span>
->>>>>>> c8d6cda3ae563bb9a380561245251d0159f5b0c8
         <button onClick={() => this.props.delete(this.props.todo.id)}>DELETE</button>
       </div>
     );
   }
 };
 
-const mapDispatchToProps = dispatch => {
-<<<<<<< HEAD
-  return {delete: band => dispatch({type: 'DELETE_BAND', payload: band})}
-=======
-  delete: band => dispatch({type: 'DELETE_BAND', payload: band})
->>>>>>> c8d6cda3ae563bb9a380561245251d0159f5b0c8
-}
 
 export default Band;
